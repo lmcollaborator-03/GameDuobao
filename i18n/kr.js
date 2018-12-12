b8a4b8ef40c6e88b876dc800555f59473c54f977
lmcollaborator-03 <41733027+lmcollaborator-03@users.noreply.github.com>
2018-12-12 14:35:49 +0800

    Update kr.js

--- i18n/kr.js
+++ i18n/kr.js
@@ -11,20 +11,20 @@ if (!window.i18n.languages) {
 window.i18n.languages['kr'] = {
     //  主游戏
     "label_text": {
-        "huodejiangli":"恭喜用户%{yonghuming}成功夺宝第%{ji}期,获得%{duoshao}奖励",
-        "daojishi":"第%{ji}期 倒计时",
-        "yigoumai":"已购买：%{ji}个",
-        "shengyu":"剩余：%{ji}个",
-        "anniu_lijizhifu":"立即支付",
-        "zichuan":"用户资产",
-        "anniu_tibi":"提币",
-        "zhanghuchongzhi":"ETH账户充值",
-        "anniu_fuzhi":"复制",
-        "benqijilu"："本期夺宝记录",
-        "xianshiquanbu"："显示全部",
-        "wodexingyunma"："我的幸运码",
-        "yonghu"："用户%{yonghuming}",
-        "chakangengduo"："查看更多",
+        "huodejiangli":"%{yonghuming}유저님이 제%{ji}회 보물파기 성공하기를 축하드립니다, %{duoshao}보상을 획득합니다.",
+        "daojishi":"제%{ji}회 카운트다운",
+        "yigoumai":"구입됨：%{ji}개",
+        "shengyu":"나머지：%{ji}개",
+        "anniu_lijizhifu":"바로 지급",
+        "zichuan":"유저 자산",
+        "anniu_tibi":"코인 출금",
+        "zhanghuchongzhi":"ETH계정 충전",
+        "anniu_fuzhi":"복사",
+        "benqijilu"："이번 회 보물파기 기록",
+        "xianshiquanbu"："더보기",
+        "wodexingyunma"："나의 행운 코드",
+        "yonghu"："%{yonghuming}유저님",
+        "chakangengduo"："더보기",
 
     }    
     
