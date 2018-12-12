68ed09ad96df9ec66702499971ec2c7cd82e7ece
7gnet <120001725@qq.com>
2018-12-12 14:06:09 +0800

    update.

--- i18n/en.js
+++ i18n/en.js
@@ -9,9 +9,23 @@ if (!window.i18n.languages) {
 }
 
 window.i18n.languages['en'] = {
-    // write your key value pairs here
+    //  主游戏
     "label_text": {
-        "ethAccount": "ETH info recharge：",
-        "purchased": "Purchased",
-    },
+        "huodejiangli":"恭喜用户%{yonghuming}成功夺宝第%{ji}期,获得%{duoshao}奖励",
+        "daojishi":"第%{ji}期 倒计时",
+        "yigoumai":"已购买：%{ji}个",
+        "shengyu":"剩余：%{ji}个",
+        "anniu_lijizhifu":"立即支付",
+        "zichuan":"用户资产",
+        "anniu_tibi":"提币",
+        "zhanghuchongzhi":"ETH账户充值",
+        "anniu_fuzhi":"复制",
+        "benqijilu"："本期夺宝记录",
+        "xianshiquanbu"："显示全部",
+        "wodexingyunma"："我的幸运码",
+        "yonghu"："用户%{yonghuming}",
+        "chakangengduo"："查看更多",
+
+    }    
+    
 };
