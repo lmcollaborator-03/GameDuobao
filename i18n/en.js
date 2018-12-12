7ad4506464470db46bf875ec61f105cd1b97b73e
7gnet <120001725@qq.com>
2018-12-12 12:50:13 +0800

    frist add.

---
+++ i18n/en.js
@@ -0,0 +1,17 @@
+'use strict';
+
+if (!window.i18n) {
+    window.i18n = {};
+}
+
+if (!window.i18n.languages) {
+    window.i18n.languages = {};
+}
+
+window.i18n.languages['en'] = {
+    // write your key value pairs here
+    "label_text": {
+        "ethAccount": "ETH info recharge：",
+        "purchased": "Purchased",
+    },
+};
